89208af3f35900ca0f57a3e3ea8a94fb4e563dc5
Stefano <stefanop21@outlook.es>
2024-05-25 17:31:00 -0500

    config: servidor

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
+const { connectionDB } = require('./database/config');
+
+// Express server
+const app = express();
+
+// Port
+const port = process.env.PORT || 3000;
+
+// Database connection
+connectionDB();
+
+// Read and parse the body of the request
+app.use(express.json());
+
+// Routes
+
+// Server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port} 🚀`);
+});
